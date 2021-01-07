8c17c8a7ce1cff6eb2ac75ffb487e75f5d9566dc
crslab <sng.weicong@gmail.com>
2021-01-07 13:39:51 +0800

    service adder

--- ros_nodes/gqcnn_rt.py
+++ ros_nodes/gqcnn_rt.py
@@ -11,130 +11,198 @@ import os
 import numpy as np
 import cv2
 
-#from tf import transformations as tft
+from std_msgs.msg import String
+
+from tf import transformations as tft
 from dougsm_helpers.timeit import TimeIt
 
-#from ggrasp.ggrasp import predict, process_depth_image
-#from dougsm_helpers.gridshow import gridshow
+from autolab_core import Point, Logger
 
-#from skimage.draw import circle
-#from skimage.feature import peak_local_max
+from gqcnn.grasping import Grasp2D, SuctionPoint2D, GraspAction
+from gqcnn.msg import GQCNNGrasp
+from gqcnn.srv import GQCNNGraspPlanner, GQCNNGraspPlannerSegmask
+from perception import BinaryImage, CameraIntrinsics, ColorImage, DepthImage
 
-#from ggrasp.msg import Grasp
 from sensor_msgs.msg import Image, CameraInfo
+from visualization import Visualizer2D as vis
+from geometry_msgs.msg import PoseStamped
+from std_msgs.msg import Header
 
 import cv_bridge
 bridge = cv_bridge.CvBridge()
 
 TimeIt.print_output = False
 
+namespace  = 'gqcnn'
+NUMBER_ITER = 5
+
 class GqCNNRt:
     def __init__(self):
         # Get the camera parameters
-        here = os.path.dirname(os.path.abspath(__file__))
-
-        value = rospy.get_param('~camera')
-        rospy.loginfo('Parameter %s has value %s', rospy.resolve_name('~foo'), value)
-
-        cam_info_topic = rospy.get_param('~camera/info_topic')
-        camera_info_msg = rospy.wait_for_message(cam_info_topic, CameraInfo)
-        self.cam_K = np.array(camera_info_msg.K).reshape((3, 3))
+        cam_info_topic = rospy.get_param('gqcnn/camera/info_topic')
+        self.camera_info_msg = rospy.wait_for_message(cam_info_topic, CameraInfo)
+        self.cam_K = np.array(self.camera_info_msg.K).reshape((3, 3))
         self.gripper = rospy.get_param("~gripper")
 
-        self.img_pub = rospy.Publisher('~visualisation', Image, queue_size=1)
-        self.cmd_pub = rospy.Publisher('~predict', Grasp, queue_size=1)
+
+        #self.img_pub = rospy.Publisher('~visualisation', Image, queue_size=1)
+        #self.cmd_pub = rospy.Publisher('~predict', Grasp, queue_size=1)
 
         self.base_frame = rospy.get_param('~camera/robot_base_frame')
         self.camera_frame = rospy.get_param('~camera/camera_frame')
         self.cam_fov = rospy.get_param('~camera/fov')
 
-        self.counter = 0
+        #self.counter = 0
         self.curr_depth_img = None
         self.curr_img_time = 0
-        self.last_image_pose = None
-        self.prev_mp = None
-        rospy.Subscriber(rospy.get_param('~camera/depth_topic'), Image, self._depth_img_callback, queue_size=1)
-
-    def _depth_img_callback(self, msg):
-        # Doing a rospy.wait_for_message is super slow, compared to just subscribing and keeping the newest one.
-        self.curr_img_time = time.time()
-        self.last_image_pose = tfh.current_robot_pose(self.base_frame, self.camera_frame)
-        if not self.last_image_pose:
-            return
-        self.curr_depth_img = bridge.imgmsg_to_cv2(msg)
-
-        depth = self.curr_depth_img.copy()
-        camera_pose = self.last_image_pose
-        cam_p = camera_pose.position
-
-        camera_rot = tft.quaternion_matrix(tfh.quaternion_to_list(camera_pose.orientation))[0:3, 0:3]
-
-        print(depth)
-
-        # # Do grasp prediction
-        # depth_crop, depth_nan_mask = process_depth_image(depth, self.img_crop_size, 300, return_mask=True, crop_y_offset=self.img_crop_y_offset)
-        # points, angle, width_img, _ = predict(depth_crop, self.model, process_depth=False, depth_nan_mask=depth_nan_mask, filters=(2.0, 2.0, 2.0))
-
-        # invalid_mask = np.zeros((300, 300)).astype("bool")
-        # if self.gripper == "robotiq":
-        #     invalid_mask[:100, :] = True
-
-        # points[invalid_mask] = 0
-
-        # # Mask Points Here
-        # angle -= np.arcsin(camera_rot[0, 1])  # Correct for the rotation of the camera
-        # angle = (angle + np.pi/2) % np.pi - np.pi/2  # Wrap [-np.pi/2, np.pi/2]
-
-        # # Convert to 3D positions.
-        # imh, imw = depth.shape
-        # x = ((np.vstack((np.linspace((imw - self.img_crop_size) // 2, (imw - self.img_crop_size) // 2 + self.img_crop_size, depth_crop.shape[1], np.float), )*depth_crop.shape[0]) - self.cam_K[0, 2])/self.cam_K[0, 0] * depth_crop).flatten()
-        # y = ((np.vstack((np.linspace((imh - self.img_crop_size) // 2 - self.img_crop_y_offset, (imh - self.img_crop_size) // 2 + self.img_crop_size - self.img_crop_y_offset, depth_crop.shape[0], np.float), )*depth_crop.shape[1]).T - self.cam_K[1,2])/self.cam_K[1, 1] * depth_crop).flatten()
-        # pos = np.dot(camera_rot, np.stack((x, y, depth_crop.flatten()))).T + np.array([[cam_p.x, cam_p.y, cam_p.z]])
-
-        # width_m = width_img / 300.0 * 2.0 * depth_crop * np.tan(self.cam_fov * self.img_crop_size/depth.shape[0] / 2.0 / 180.0 * np.pi)
-
-        # maxes = peak_local_max(points, min_distance=10, threshold_abs=0.1, num_peaks=3)
-
-        # if maxes.shape[0] == 0:
-        #     rospy.logerr("No Local Maxes")
-        #     return
+        #self.last_image_pose = None
+        #self.prev_mp = None
+        #rospy.Subscriber(rospy.get_param('~camera/depth_topic'), Image, self._depth_img_callback, queue_size=1)
         
-        # if self.prev_mp is None:
-        #     best_g_unr = maxes[np.argmin(np.linalg.norm(maxes, axis=1))]
-        #     self.prev_mp = best_g_unr
-        # else:
-        #     best_g_unr = maxes[np.argmin(np.linalg.norm(maxes - self.prev_mp, axis=1))]
-        #     self.prev_mp = (best_g_unr * 0.25 + self.prev_mp * 0.75).astype(np.int)
-        # best_g = np.ravel_multi_index(((best_g_unr[0]), (best_g_unr[1])), points.shape)
-        # best_g_unr = np.unravel_index(best_g, points.shape)
+        # For getting camera pose
+        #self.last_image_pose = tfh.current_robot_pose(self.base_frame, self.camera_frame)
+        # camera_pose = self.last_image_pose
+        # cam_p = camera_pose.position
+        # camera_rot = tft.quaternion_matrix(tfh.quaternion_to_list(camera_pose.orientation))[0:3, 0:3]
 
-        # g = Grasp()
-        # g.pose.position.x = pos[best_g, 0]
-        # g.pose.position.y = pos[best_g, 1]
-        # g.pose.position.z = pos[best_g, 2]
-        # g.pose.orientation = tfh.list_to_quaternion(tft.quaternion_from_euler(np.pi, 0, ((angle[best_g_unr]%np.pi) - np.pi/2)))
-        # g.width = width_m[best_g_unr]
-        # g.quality = points[best_g_unr]
+        # DEXNET SRV COMM:
+        rospy.wait_for_service("%s/grasp_planner" % (namespace))
+        rospy.wait_for_service("%s/grasp_planner_segmask" % (namespace))
+        self.plan_grasp = rospy.ServiceProxy("%s/grasp_planner" % (namespace),
+                                    GQCNNGraspPlanner)
 
-        # grasp_img = cv2.applyColorMap((points * 255).astype(np.uint8), cv2.COLORMAP_JET)
-        # rr, cc = circle(self.prev_mp[0], self.prev_mp[1], 5)
-        # grasp_img[rr, cc, 0] = 0
-        # grasp_img[rr, cc, 1] = 255
-        # grasp_img[rr, cc, 2] = 0
+        self.ok = True
+        self.get_grasp = False
 
-        # show = gridshow('Display',
-        #          [depth_crop, grasp_img],
-        #          [(0.30, 0.55), None, (-np.pi/2, np.pi/2)],
-        #          [cv2.COLORMAP_BONE, cv2.COLORMAP_JET, cv2.COLORMAP_BONE],
-        #          3,
-        #          False)
 
-        # self.img_pub.publish(bridge.cv2_to_imgmsg(show))
-        # self.cmd_pub.publish(g)
+        self.grasp_publisher = rospy.Publisher('~grasp_output', PoseStamped, queue_size=10)
+        rospy.Subscriber('~grasp_input', String, self.command_callback)
+
+    def command_callback(self, msg):
+        if msg.data == 'calculate_grasp':
+            self.get_grasp = True
+        elif msg.data == 'stop':
+            self.get_grasp = False
+            self.ok = False
+        else:
+            print('Bad command')
+
+    def go(self):
+        depth_raw = rospy.wait_for_message(rospy.get_param('~camera/depth_topic'), Image)
+        depth = bridge.imgmsg_to_cv2(depth_raw)
+
+        depth_im = DepthImage(depth, frame=self.camera_frame)
+        color_im = ColorImage(np.zeros([depth_im.height, depth_im.width,
+                                   3]).astype(np.uint8),
+                         frame=self.camera_frame)
+
+        
+        grasp_resp = self.plan_grasp(color_im.rosmsg, depth_im.rosmsg,
+                                self.camera_info_msg)
+        grasp = grasp_resp.grasp
+        grasp_type = grasp.grasp_type
+        center = Point(np.array([grasp.center_px[0], grasp.center_px[1]]),
+               frame=self.camera_frame)
+
+        print(self.camera_info_msg)
+        print(self.cam_K.shape)
+
+        camera_intr = CameraIntrinsics("primesense_overhead",
+                                                fx=self.cam_K[0][0],
+                                                fy=self.cam_K[1][1],
+                                                cx=self.cam_K[0][2],
+                                                cy=self.cam_K[1][2],
+                                                width=int(self.camera_info_msg.width),
+                                                height=int(self.camera_info_msg.height))
+
+        grasp_2d = Grasp2D(center,
+                           grasp.angle,
+                           grasp.depth,
+                           width=0.08,
+                           camera_intr=camera_intr)
+
+        thumbnail = DepthImage(bridge.imgmsg_to_cv2(
+            grasp.thumbnail, desired_encoding="passthrough"),
+                               frame=self.camera_frame)
+        #print(grasp.q_value)
+        action = GraspAction(grasp_2d, grasp.q_value, thumbnail)
+
+        #vis.figure(size=(10, 10))
+        # vis.imshow(depth_im, vmin=0.6, vmax=0.9)
+        # vis.imshow(depth_im)
+
+        # vis.grasp(action.grasp, scale=2.5, show_center=False, show_axis=True)
+        # vis.title("Planned grasp on depth %.3f (Q=%.3f)" % (grasp.depth, action.q_value))
+        # vis.show()
+
+        return grasp_2d, action
+
+    def publish_grasp(self, grasp):
+        # Create `GQCNNGrasp` return msg and populate it.
+        gqcnn_grasp = GQCNNGrasp()
+        gqcnn_grasp.q_value = grasp.q_value
+        gqcnn_grasp.pose = grasp.grasp.pose().pose_msg
+        if isinstance(grasp.grasp, Grasp2D):
+            gqcnn_grasp.grasp_type = GQCNNGrasp.PARALLEL_JAW
+        elif isinstance(grasp.grasp, SuctionPoint2D):
+            gqcnn_grasp.grasp_type = GQCNNGrasp.SUCTION
+        else:
+            rospy.logerr("Grasp type not supported!")
+            raise rospy.ServiceException("Grasp type not supported!")
+
+        # Store grasp representation in image space.
+        gqcnn_grasp.center_px[0] = grasp.grasp.center[0]
+        gqcnn_grasp.center_px[1] = grasp.grasp.center[1]
+        gqcnn_grasp.angle = grasp.grasp.angle
+        gqcnn_grasp.depth = grasp.grasp.depth
+        gqcnn_grasp.thumbnail = grasp.image.rosmsg
+
+        # Create and publish the pose alone for easy visualization of grasp
+        # pose in Rviz.
+        pose_stamped = PoseStamped()
+
+        # pose is taken from Grasp2D method. Returns pose in camera frame of reference(KIV)
+        pose_stamped.pose = grasp.grasp.pose().pose_msg
+
+        # Use simpler method for getting pose
+        #pose = np.linalg.inv(cam_K)*np.array([gqcnn_grasp.center_px[0], gqcnn_grasp.center_px[1], 1.0]) + np.array([cam_p.x, cam_p.y, cam_p.z])
+        header = Header()
+        header.stamp = rospy.Time.now()
+        header.frame_id = self.camera_frame
+        pose_stamped.header = header
+        self.grasp_publisher.publish(pose_stamped)
+
+    def run(self):
+        while(self.ok):
+
+            if self.get_grasp:
+                q_values = []
+                actions = []
+                grasps = []
+                for i in range(NUMBER_ITER):
+                    print('Iteration: ', i+1)
+                    grasp_2d, action = self.go()
+                    q_values.append(action.q_value)
+                    grasps.append(grasp_2d)
+                    actions.append(action)
+
+                best_indx = np.argmax(np.array(q_values))
+                best_grasp = grasps[best_indx]
+                best_action = actions[best_indx]
+                self.get_grasp = False
+
+                print('Q values:', q_values)
+                print('Best q', best_action.q_value)
+
+                # publish to the main planner
+                self.publish_grasp(action)
+
+            rospy.sleep(1)
 
 
 if __name__ == '__main__':
     rospy.init_node('gqcnn', anonymous=True)
     import dougsm_helpers.tf_helpers as tfh
     GqCNNRt = GqCNNRt()
-    rospy.spin()
+    GqCNNRt.run()
+    #rospy.spin()
